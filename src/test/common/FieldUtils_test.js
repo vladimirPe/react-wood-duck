614a9a10bc65f6a6d5c92d911f2b8f4347c23ddc
Volodymyr Petrusha <volodymyr.petrusha@osi.ca.gov>
2018-05-01 12:02:02 -0700

    tests were refactored added tests for undefined

--- src/test/common/FieldUtils_test.js
+++ src/test/common/FieldUtils_test.js
@@ -1,23 +1,40 @@
 import { sanitizeValue } from '../../common/FieldUtils';
 
-describe('Field utils', () => {
-  describe('Sanitize Value', () => {
-    it('Value not null', () => {
-      expect(sanitizeValue('a1234a', '[0-9]')).toBe('1234');
+describe('FieldУtils', () => {
+  describe('#santizeValue', () => {
+    describe('When provide a string value', () => {
+      it('returns sanitised result', () => {
+        expect(sanitizeValue('a1234a', '[0-9]')).toBe('1234');
+      });
     });
-
-    it('Value is empty', () => {
-      expect(sanitizeValue('', '[0-9]')).toBe('');
+    describe('When provides an empty value', ()=>{
+      it('returns empty string', () => {
+        expect(sanitizeValue('', '[0-9]')).toBe('');
+      });
     });
-
-    it('Value is null', () => {
-      expect(sanitizeValue(null, '[0-9]')).toBe('');
+    describe('When provides a null', () => {
+      it('returns empty string', () => {
+        expect(sanitizeValue(null, '[0-9]')).toBe('');
+      });
     });
-
-    it('Regex null', () => {
-      expect(() => {
-        sanitizeValue('', null);
-      }).toThrow(new Error('regex must not be null'));
+    describe('When provides a undefined', () => {
+      it('returns empty string', () => {
+        expect(sanitizeValue(undefined, '[0-9]')).toBe('');
+      });
+    });
+    describe('When provides regexp as null', ()=>{
+      it('Throws error', () => {
+        expect(() => {
+          sanitizeValue('', null);
+        }).toThrow(new Error('regex must not be null'));
+      });
+    });
+    describe('When provides regexp as undefined', ()=>{
+      it('Throws error', () => {
+        expect(() => {
+          sanitizeValue('', undefined);
+        }).toThrow(new Error('regex must not be null'));
+      });
     });
   });
 });
